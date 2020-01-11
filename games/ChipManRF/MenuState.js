f5286a7890539c5ec3fb9ecf3ffb4819eb346e07
Max <klinman.max@gmail.com>
2020-01-10 18:13:00 -0800

    Re-organized MenuState so that I don't go crazy

--- games/ChipManRF/MenuState.js
+++ games/ChipManRF/MenuState.js
@@ -4,7 +4,10 @@ import { GameState } from "./GameState.js";
 import { PreloadState } from "./PreloadState.js";
 
 
-class MenuState extends Phaser.Scene {
+export class MenuState extends Phaser.Scene {
+
+  /* ===== BLUE-PRINT FUNCTIONS ===== */
+
   constructor() {
     super({key: "MenuState"});
   }
@@ -24,92 +27,166 @@ class MenuState extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(0, 0, "titleGraphic");
-    // Free Phaser's canvas for Three.js's use
+    /* === Class Variables === */
+    // The chipman spine that is shown running around on the title screen
+    this.chipman = this.add.spine(0, -222, "chipman");
+    this.chipman.setScale(0.1, 0.1);
+
+    // A mostly empty container that only holds chipman
+    // Phaser strongly reccomends against this, but until I can rotate
+    //  or anchor a spine object, this is my only option.
+    this.chipmanContainer = this.add.container(0, 0, this.chipman)
+
+    // Holds the moon and everything on it (to rotate slowly)
+    // This includes chipman's container!
+    this.moonContainer = this.add.container(100, 450);
+    this.moonContainer.depth = 1;
+
+    /* === Local Variables === */
+    // Black background (space ya'know)
+    let background = this.add.graphics();
+    background.fillStyle(0x000000);
+    background.fillRect(0, 0, 640, 480);
+    background.depth = 0;
+
+    // Chipman's house that will live and rotate on the moon
+    let house = this.add.image(0, 0, "house");
+    house.setScale(0.5, 0.5);
+    house.setOrigin(.5, 3.27);
+    // Depth is set in the order of the add array
+
+    // The moon game object
+    let moon = this.add.image(0, 0, "moon");
+    moon.setScale(0.5, 0.5);
+    // Depth is set in the order of the add array
+
+    // Add the menu options
+    let startButton = this.add.image(460, 240, "startButton");
+    startButton.setScale(0.5, 0.5);
+    startButton.setInteractive();
+    startButton.on("pointerup", this.startGame.bind(this));
+    startButton.depth = 5;
+
+    // The name of the game all fancy like
+    let title = this.add.image(430, 100, "titleGraphic");
+    title.setScale(0.5, 0.5);
+    title.depth = 5;
+
+    /* === Action === */
+    // Add the moon bound objects to the moon
+    this.moonContainer.add([house, moon, this.chipmanContainer]);
+
+    // The scene is now fully set up, I do this before the three.js hand-off
+    // to reduce any intialization lag when the game switchs back to Phaser.
+
+    // Prepare the hand over to Three.JS
     this.game.renderer.clearPipeline();
     this.game.loop.sleep();
     CanvasControl.freeCanvas(this.game.renderer.canvas,
+                             // Make there there's a way back to Phaser
                              this.resumeControlFromThreeJS.bind(this));
     // Notify THREE.JS everything is good to go
     OpenScene3D.startOpenCinematic();
   }
 
-  resumeControlFromThreeJS() {
-    // Reclaim the canvas
-    CanvasControl.restrainCanvas();
-    this.game.loop.wake(true);
+  update() {
 
-    let background = this.add.graphics();
-    background.fillStyle(0x000000);
-    background.fillRect(0, 0, 640, 480);
+  }
 
-    let title = this.add.image(430, 100, "titleGraphic");
-    title.setScale(0.5, 0.5);
+  /* ===== ACTION FUNCTIONS ===== */
 
-    let house = this.add.image(0, 0, "house");
-    house.setScale(0.5, 0.5);
-    house.setOrigin(.5, 3.27);
+  /*
+   * chipmanNextAction()
+   * Will create a Phaser Tween based on what action is randomly decided upon
+   * Special cases favor certain directions over others
+   */
+  chipmanNextAction() {
+    let actionNames = this.chipmanActions.names;
 
-    // Get ChipMan in on the action
-    let chipman = this.add.spine(0, -222, "chipman");
-    chipman.setScale(0.1, 0.1);
+    if (this.chipmanContainer.angle + this.moonContainer.angle % 360 < -30 ||
+        this.chipmanContainer.angle + this.moonContainer.angle % 360 > 270) {
+      let distance = Math.random() * (60) + 40;
+      this.chipmanActions["runRight"](distance);
+      // Randomly jumps
+      if (Math.random() > 0.75) {
+        this.chipmanActions["jump"]();
+      }
+    }
+    else if (this.chipmanContainer.angle + this.moonContainer.angle % 360 > 70) {
+      let distance = Math.random() * (60) + 40;
+      this.chipmanActions["runLeft"](distance);
+      // Randomly jumps
+      if (Math.random() > 0.75) {
+        this.chipmanActions["jump"]();
+      }
+    }
+    else {
+      let action = Math.floor(Math.random() * 3);
+      if (action < 2) { // distance
+        let distance = Math.random() * (60) + 40;
+        this.chipmanActions[actionNames[action]](distance); // give random distance
+        // Randomly jumps
+        if (Math.random() > 0.75) {
+          this.chipmanActions["jump"]();
+        }
+      } else {
+        // Stall
+        this.chipmanActions[actionNames[action]](Math.random() * (900) + 100); // random time
+      }
+    }
+  }
 
-    let chipmanContainer = this.add.container(0, 0, chipman)
+  /*
+   * generateChipManActions() -> object{names: array[string], *: function() ... }
+   * Returns a large object with functions that define menu screen
+   * animations for chipman.
+   */
+  generateChipManActions() {
+    return {
+      // Have a list of the name of each function
+      // Order should be consistent and constant
+      names: ["runLeft", "runRight", "stall", "jump"],
 
-    let moon = this.add.image(0, 0, "moon");
-    moon.setScale(0.5, 0.5);
-
-    let moonContainer = this.add.container(100, 450, [house, moon, chipmanContainer]);
-
-    // All things on the "rotating" moon should go here
-    this.add.tween({
-      targets: moonContainer,
-      angle: 360,
-      duration: 70000,
-      loop: -1,
-    });
-
-    let chipmanActions = {
       runLeft: (distance) => {
-        chipman.scaleX = -.1;
-        chipman.setAnimation(0, "run", true);
+        this.chipman.scaleX = -.1;
+        this.chipman.setAnimation(0, "run", true);
         this.add.tween({
-          targets: chipmanContainer,
-          angle: chipmanContainer.angle - distance,
+          targets: this.chipmanContainer,
+          angle: this.chipmanContainer.angle - distance,
           duration: distance / 0.02, // 0.02 is the speed
-          onComplete: chipmanNextAction,
+          onComplete: this.chipmanNextAction,
           onCompleteScope: this,
         })
       },
       runRight: (distance) => {
-        chipman.scaleX = .1;
-        chipman.setAnimation(0, "run", true);
+        this.chipman.scaleX = .1;
+        this.chipman.setAnimation(0, "run", true);
         this.add.tween({
-          targets: chipmanContainer,
-          angle: chipmanContainer.angle + distance,
+          targets: this.chipmanContainer,
+          angle: this.chipmanContainer.angle + distance,
           duration: distance / 0.02, // 0.02 is the speed
-          onComplete: chipmanNextAction,
+          onComplete: this.chipmanNextAction,
           onCompleteScope: this,
         })
       },
       stall: (time) => {
-        chipman.setAnimation(0, "idle", true);
+        this.chipman.setAnimation(0, "idle", true);
         this.time.addEvent({
           delay: time,
-          callback: chipmanNextAction,
+          callback: this.chipmanNextAction,
           callbackScope: this,
         })
       },
       jump: () => {
         this.add.tween({
-          targets: chipman,
+          targets: this.chipman,
           y: -350,
           duration: 1000, // 0.02 is the speed
           ease: Phaser.Math.Easing.Quadratic.Out,
           onCompleteScope: this,
           onComplete: () => {
             this.add.tween({
-              targets: chipman,
+              targets: this.chipman,
               y: -222,
               duration: 1000, // 0.02 is the speed
               ease: Phaser.Math.Easing.Quadratic.In,
@@ -118,64 +195,41 @@ class MenuState extends Phaser.Scene {
         });
       }
     };
-
-    let actions = ["runLeft", "runRight", "stall", "jump"]
-    function chipmanNextAction() {
-      if (chipmanContainer.angle + moonContainer.angle % 360 < -30 ||
-          chipmanContainer.angle + moonContainer.angle % 360 > 270) {
-        let distance = Math.random() * (60) + 40;
-        chipmanActions["runRight"](distance);
-        // Randomly jumps
-        if (Math.random() > 0.75) {
-          chipmanActions["jump"]();
-        }
-      }
-      else if (chipmanContainer.angle + moonContainer.angle % 360 > 70) {
-        let distance = Math.random() * (60) + 40;
-        chipmanActions["runLeft"](distance);
-        // Randomly jumps
-        if (Math.random() > 0.75) {
-          chipmanActions["jump"]();
-        }
-      }
-      else {
-        let action = Math.floor(Math.random() * 3);
-        if (action < 2) { // distance
-          let distance = Math.random() * (60) + 40;
-          chipmanActions[actions[action]](distance); // give random distance
-          // Randomly jumps
-          if (Math.random() > 0.75) {
-            chipmanActions["jump"]();
-          }
-        } else {
-          // Stall
-          chipmanActions[actions[action]](Math.random() * (900) + 100); // random time
-        }
-      }
-    }
-
-    chipmanNextAction();
-
-    // Add the menu options
-    let startButton = this.add.image(460, 240, "startButton");
-    startButton.setScale(0.5, 0.5);
-    startButton.setInteractive();
-    startButton.on("pointerup", startGame.bind(this));
-
-    function startGame() {
-      // Fix this later (it is a game crash)
-      //chipman.destroy();
-      this.game.scene.stop("MenuState");
-      this.game.scene.add("GameState", GameState, true);
-    }
   }
 
-  update() {
+  /*
+   * resumeControlFromThreeJS()
+   * Will get the actions going on the menu.
+   */
+  resumeControlFromThreeJS() {
+    // Reclaim the canvas
+    CanvasControl.restrainCanvas();
+    this.game.loop.wake(true);
 
+    // All things on the "rotating" moon should go here
+    this.add.tween({
+      targets: this.moonContainer,
+      angle: 360,
+      duration: 70000,
+      loop: -1,
+    });
+
+    this.chipmanActions = this.generateChipManActions();
+
+    this.chipmanNextAction();
+  }
+
+  /*
+    startGame()
+    Shuts down the main menu and begins the game proper.
+  */
+  startGame() {
+    this.game.scene.stop("MenuState");
+    this.game.scene.add("GameState", GameState, true);
   }
 
   // Temp (only keeping here for later reference)
-  titleGraphicAnimation(titleGraphic) {
+  /*titleGraphicAnimation(titleGraphic) {
     titleGraphic.setTint(0x000000);
     // Fade in title (old fashion method)
     let titleTimeline = this.tweens.createTimeline();
@@ -235,7 +289,5 @@ class MenuState extends Phaser.Scene {
       scaleY: 20,
       ease: Phaser.Math.Easing.Expo.In
     })
-  }
+  }*/
 }
-
-export { MenuState };
